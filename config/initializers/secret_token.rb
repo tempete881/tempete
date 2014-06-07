84766594c16239c80fca862a65d4d66415075f99
z-payment <fedotova@pokupo.ru>
2014-06-07 21:03:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project1::Application.config.secret_key_base = '5f50c22cbce388eb5126fc152014b50f177b5b2789f9bfe38ac17ec94e8ddb75ae1921e2f42cd062280332be24ab544076b706ee577de33bddbe84707f406c92'
